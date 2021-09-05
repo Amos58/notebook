5fb30f36506ea96fccc9bbcac3d6e9ac6b7dc7a8
Amos <55913276+Amos58@users.noreply.github.com>
2021-09-05 17:10:52 +0800

    Add files via upload

---
+++ 学的一些技能啊/tensorflow2.6/class.py
@@ -0,0 +1,46 @@
+import tensorflow as tf 
+import numpy as np
+from sklearn import datasets
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Model
+
+#导入数据
+x_train = datasets.load_iris().data
+y_train=datasets.load_iris().target
+
+#打乱数据集
+np.random.seed(42)
+np.random.shuffle(x_train)
+np.random.seed(42)
+np.random.shuffle(y_train)
+tf.random.set_seed(42)
+
+#model Sequential
+#model=tf.keras.models.Sequential([
+#    tf.keras.layers.Dense(3,activation='softmax',kernel_regularizer=tf.keras.regularizers.l2())
+#])
+
+#class
+class IrisModel(Model):
+    def __init__(self):
+        super(IrisModel,self).__init__()
+        self.d1=Dense(3,activation='softmax',kernel_regularizer=tf.keras.regularizers.l2())
+    
+    def call(self, x):
+        y=self.d1(x)
+        return y
+
+model=IrisModel()
+
+#model compile
+model.compile(optimizer=tf.keras.optimizers.Adam(lr=0.1),
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=False),
+              metrics=['sparse_categorical_accuracy'])
+
+#model fit
+model.fit(x_train, y_train,batch_size=32,epochs=500,validation_split=0.2,validation_freq=20)
+
+#summary
+model.summary()
+
+
